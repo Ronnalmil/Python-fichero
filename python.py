e77a491b1a927f5d6d3d4dee480537ccca363a51
ronnytron <ronnal2020@gmail.com>
2025-05-02 21:20:04 +0200

    Crear python.py

---
+++ python.py
@@ -0,0 +1,16 @@
+#
+
+import random
+
+numero_secreto=random.randint(1,10)
+intento = 0
+
+while true:
+      if intento < numero_secreto 
+         print("es esta abajo")
+      if intento > numero_secreto
+         print("está por encima")
+      else:
+        print ("correcto su numero ",numero_secreto)
+        break
+
