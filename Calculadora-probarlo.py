96eafa4ab04dc902f098f89a53bd36aa2bfa9f5f
ronnytron <ronnal2020@gmail.com>
2025-05-02 21:42:40 +0200

    Crear Calculadora-probarlo.py

---
+++ Calculadora-probarlo.py
@@ -0,0 +1,36 @@
+#
+
+num1=float(input("primero numero")
+num2=float(input("segundo numero")
+
+print("1.sumar")  
+print("2.restar")
+print("3.multiplicar")
+print("4.dividir")
+
+opcion = print("elija 1/2/3/4")
+
+if opcion == "1":
+   print("resultado:", num1 + num2)
+elif  opcion == "2":
+   print("resultado:", num1 - num2)
+elif  opcion == "3":
+   print("resultado:", num1 * num2)
+elif  opcion == "4":
+      if num2 !=0:
+         print("resultado", num1 / num2)
+      else: 
+         print("error")
+
+
+
+
+
+
+
+
+
+
+
+
+      
