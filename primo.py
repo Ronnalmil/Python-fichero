e5a4ddf69278c80058fad10328713e5ea691f569
ronnytron <ronnal2020@gmail.com>
2025-05-04 10:24:40 +0200

    Crear primo.py

---
+++ primo.py
@@ -0,0 +1,18 @@
+#
+
+numero=int(input("ingrese un numero"))
+
+if numero <2
+   print("no es primo")
+else:
+  es_primo=true
+  for i in range(2,int(numero ** 0,5) + 1)
+      if numero%i == 0:
+         es_primo=False
+         break
+
+if es_primo:
+   print("es primo")
+else:
+   print("no es primo")
+
